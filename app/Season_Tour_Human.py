1706b12657a364b42fd93bf3a9d6c29bbce53684
HA TAE UK <hive0917@gmail.com>
2023-06-06 02:11:52 +0900

    Update Season_Tour_Human.py
    불필요내역 삭제

--- app/Season_Tour_Human.py
+++ app/Season_Tour_Human.py
@@ -1,8 +1,8 @@
+import pandas as pd
+import seaborn as sns  # pip install seaborn
+import matplotlib.pyplot as plt  # pip install matplotlib
+import mpld3
 def HUMANDATA(CITY_PARM):
-    import pandas as pd
-    import seaborn as sns #pip install seaborn
-    import matplotlib.pyplot as plt #pip install matplotlib
-    import mpld3
 
     plt.rcParams['font.family'] = 'Malgun Gothic'
     Citydata_filename = fr'Citydata_2023-05-20.csv'
@@ -23,28 +23,31 @@ def HUMANDATA(CITY_PARM):
             return '겨울'
 
 
-    def Human_data_3_years(input_city):
-        for city in [x for x in City_list if input_city in x]:
-            #3년치 유동인구 데이터 병합
-            csv_file_2020 = fr"2020_holidayandseason_data\2020_result_data({city}).csv"
-            csv_file_2021 = fr"2021_holidayandseason_data\2021_result_data({city}).csv"
-            csv_file_2022 = fr"2022_holidayandseason_data\2022_result_data({city}).csv"
-            csv_files = [csv_file_2020,csv_file_2021,csv_file_2022]
-            HolidayAndSeason_df = [pd.read_csv(file, encoding='utf-8') for file in csv_files]
-            HolidayAndSeason_Merge_df = pd.concat(HolidayAndSeason_df, ignore_index=True)
-            HolidayAndSeason_Merge_df.set_index('날짜', inplace=True)
-            HolidayAndSeason_Merge_df['월'] = pd.to_datetime(HolidayAndSeason_Merge_df.index).month
+    def Human_data_3_years(city):
+        #3년치 유동인구 데이터 병합
+        csv_file_2020 = fr"2020_holidayandseason_data\2020_result_data({city}).csv"
+        csv_file_2021 = fr"2021_holidayandseason_data\2021_result_data({city}).csv"
+        csv_file_2022 = fr"2022_holidayandseason_data\2022_result_data({city}).csv"
+        csv_files = [csv_file_2020,csv_file_2021,csv_file_2022]
+        HolidayAndSeason_df = [pd.read_csv(file, encoding='utf-8') for file in csv_files]
+        HolidayAndSeason_Merge_df = pd.concat(HolidayAndSeason_df, ignore_index=True)
+        HolidayAndSeason_Merge_df.set_index('날짜', inplace=True)
+        HolidayAndSeason_Merge_df['월'] = pd.to_datetime(HolidayAndSeason_Merge_df.index).month
 
-            #데이터 정제
-            HolidayAndSeason_Merge_df['계절'] = HolidayAndSeason_Merge_df['월'].apply(get_season)
-            correlation = HolidayAndSeason_Merge_df.groupby('계절')['유동인구수'].mean().reset_index()
+        #데이터 정제
+        HolidayAndSeason_Merge_df['계절'] = HolidayAndSeason_Merge_df['월'].apply(get_season)
+        correlation = HolidayAndSeason_Merge_df.groupby('계절')['유동인구수'].mean().reset_index()
 
-            #가시화
-            fig = plt.figure(figsize=(8, 6))
-            sns.barplot(data=correlation, x='계절', y='유동인구수', palette='Set1')
-            plt.title('계절별 유동인구수 평균')
-            plt.xlabel('계절')
-            plt.ylabel('유동인구수')
-            plt.show()
-            mpld3.save_html(fig, fr"{city}_Season_Tour_Human.html")
-    Human_data_3_years(input_city)
+        #리턴데이터 형성
+        Spring = int(correlation[correlation['계절'] == '봄']['유동인구수'].values[0])
+        Summer = int(correlation[correlation['계절'] == '여름']['유동인구수'].values[0])
+        Fall = int(correlation[correlation['계절'] == '가을']['유동인구수'].values[0])
+        Winter = int(correlation[correlation['계절'] == '겨울']['유동인구수'].values[0])
+        DATA = {city: {"Spring": Spring, "Summer": Summer, "Fall": Fall, "Winter": Winter}}
+        return DATA
+
+    DATA_list = []
+    for city in [x for x in City_list if input_city in x]:
+        DATA = Human_data_3_years(city)
+        DATA_list.append(DATA)
+    return DATA_list
